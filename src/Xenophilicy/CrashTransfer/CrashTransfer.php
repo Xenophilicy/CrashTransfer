2c02bb99f06f4f04e6a4fd28fe3009b78c9953a8
Vecnavium <vecnavium@gmail.com>
2021-10-30 16:41:12 -0700

    Updated to API 4.0.0

--- src/Xenophilicy/CrashTransfer/CrashTransfer.php
+++ src/Xenophilicy/CrashTransfer/CrashTransfer.php
@@ -16,7 +16,7 @@
 namespace Xenophilicy\CrashTransfer;
 
 use pocketmine\event\Listener;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
@@ -27,23 +27,31 @@ use pocketmine\utils\Config;
 class CrashTransfer extends PluginBase implements Listener {
     
     public static $settings;
-    public $taskIDs;
+
+    public function onLoad(): void
+    {
+        $this->messages = new Config(
+            $this->getFile() . "resources/languages/" . $this->getConfig()->get("language", "en") . ".yml"
+        );
+    }
     
-    public function onEnable(){
+    public function onEnable():void
+    {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveDefaultConfig();
         $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
         self::$settings = $this->config->getAll();
         if(!is_numeric(self::$settings["Server"]["Port"])){
-            $this->getLogger()->critical("Target server port must be numeric. Plugin will remain disabled...");
+            $this->getLogger()->critical($this->getMessage("error.serveripconfig"));
             $this->getServer()->getPluginManager()->disablePlugin($this);
         }
-        $this->getLogger()->info("Transferring players to " . self::$settings["Server"]["Address"] . ":" . self::$settings["Server"]["Port"] . " on server stop");
+        $this->getLogger()->info($this->getMessage("general.transferingplayers") . self::$settings["Server"]["Address"] . ":" . self::$settings["Server"]["Port"]);
     }
     
-    public function onDisable(){
+    public function onDisable(): void
+    {
         if($this->getServer()->isRunning()) return;
-        $players = $this->getServer()->getLoggedInPlayers();
+        $players = $this->getServer()->getOnlinePlayers();
         if(sizeof($players) === 0) return;
         if(!self::$settings["Warning"]["Enabled"] || self::$settings["Warning"]["Delay"] <= 0){
             $this->transferPlayers($players);
@@ -66,12 +74,27 @@ class CrashTransfer extends PluginBase implements Listener {
      * @param array $players
      */
     public function transferPlayers(array $players){
-        $this->getLogger()->notice("Transferring players...");
+        $this->getLogger()->info($this->getMessage("general.transferinginprogress1"));
         foreach($players as $player){
             if(!$player instanceof Player) continue;
             $player->transfer(self::$settings["Server"]["Address"], self::$settings["Server"]["Port"]);
-            $this->getLogger()->notice("Transferring " . $player->getName());
+            $this->getLogger()->info($this->getMessage("general.transferinginprogress2") . $player->getName());
         }
-        $this->getLogger()->notice("All players have been transferred");
+        $this->getLogger()->info($this->getMessage("general.transferedplayers"));
     }
+
+    public function getMessage(string $key, array $replaces = array()): string {
+        if($rawMessage = $this->messages->getNested($key)) {
+            if(is_array($replaces)) {
+                foreach($replaces as $replace => $value) {
+                    $rawMessage = str_replace("{" . $replace . "}", $value, $rawMessage);
+                }
+            }
+
+            return $rawMessage;
+        }
+
+        return $key;
+    }
+
 }
